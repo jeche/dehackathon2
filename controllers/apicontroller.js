141b240a4666eb79863afe7152830ddeeab9109b
Matt Strapko <okparts@gmail.com>
2014-02-06 15:17:29 -0500

    some js files added

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,16 +1,16 @@
 function get_cycle_list(minu, maxi){
 	var ac;
 	var result;
-	$.ajax("https://api.traderonline.com/vlatest/token", {
+	$.ajax("https://apis.traderonline.com/vlatest/token", {
 		type:"POST",
 		data: {
-				client_id:"william-and-mary",
-				client_secret:"william-and-mary",
-				grant_type:"client_credentials"},
+				client_id: secret_things.client_id,
+				client_secret:secret_things.client_secret,
+				grant_type:secret_things.grant_type},
 		success:function(data) {
 			console.log(data);
 			ac = data.access_token;
-			$.ajax("https://api-dev.traderonline.com/vlatest/cycles", {
+			$.ajax("https://apis.traderonline.com/vlatest/cycles", {
 					type:"GET",
 					headers: {"Authorization" : "Bearer " + data.access_token},
 					data:{  
@@ -23,7 +23,7 @@ function get_cycle_list(minu, maxi){
 					success:function(data){
 						console.log(data);
 						for(i = 0; i < data.result.length; i++){
-							$.ajax("https://api-dev.traderonline.com/vlatest/cycles/" + data.result[i].id, {
+							$.ajax("https://apis.traderonline.com/vlatest/cycles/" + data.result[i].id, {
 								type:"GET",
 								headers: {"Authorization" : "Bearer " + ac},
 								success:function(data){
--- javascripts/itemInfoMessage.js
+++ javascripts/itemInfoMessage.js
@@ -1,11 +1,4 @@
-function popup(mylink, windowname)
-{
-if (! window.focus)return true;
-var href;
-if (typeof(mylink) == 'string')
-   href=mylink;
-else
-   href="../views/itemInfoMessage.html";
-window.open(href, 'notes', 'width=550,height=700,scrollbars=yes');
-return false;
+
+function getData(){
+	
 }
