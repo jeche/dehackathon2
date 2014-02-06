eaf263d0173d33961a0fda639a0c5c15dd440e87
Matt Strapko <okparts@gmail.com>
2014-02-06 14:45:46 -0500

    New pop up style

--- javascripts/itemInfoMessage.js
+++ javascripts/itemInfoMessage.js
@@ -5,7 +5,7 @@ var href;
 if (typeof(mylink) == 'string')
    href=mylink;
 else
-   href=mylink.href;
-window.open(href, windowname, 'width=550,height=700,scrollbars=yes');
+   href="../views/itemInfoMessage.html";
+window.open(href, 'notes', 'width=550,height=700,scrollbars=yes');
 return false;
 }
