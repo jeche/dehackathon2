16e842dfc33d304c5b3643094d63ffec3bdb5d0a
Matt Strapko <okparts@gmail.com>
2014-02-06 21:43:23 -0500

    updates not work

--- javascripts/itemInfoMessage.js
+++ javascripts/itemInfoMessage.js
@@ -1,45 +1,45 @@
 function getData(){
 	
-	var Data = get_cycle_list(0.0, 300000.0);
-	if(Data === null) return true;
+	var data = localStorage.getItem("bikeArray");
 	var lowPrice = 100000000;
 	var highPrice = 0;
 	var l = 0, h = 0;
 
-for(var i = 0; i < Data.length; i++){
+for(var i = 0; i < data.length; i++){
 	if(data[i].price < lowPrice){
-		lowPrice = Data[i].price;
+		lowPrice = data[i].price;
 		l = i;
 	}
-	if(Data[i].price > highPrice){
-		highPrice = Data[i].price;
+	if(data[i].price > highPrice){
+		highPrice = data[i].price;
 		h = i;
 	}
-}
+}										
+	var item1 = document.getElementById("item1");
+	item1.textContent = data[h].makeDisplayName;
+	var item2 = document.getElementById("item2");
+	item2.textContent = data[l].makeDisplayName;
 
-var item1 = document.getElementById("item1");
-item1.textContent = data[l].makeDisplayName;
+	/*var price1 = document.getElementById("price1");
+	price1.textContent = resultArray[0].price;
 
-var item2 = document.getElementById("item2");
-item2.textContent = data[h].makeDisplayName;
+	var price2 = document.getElementById("price2");
+	price2.textContent = resultArray[1].price;
 
-var price1 = document.getElementById("price1");
-price1.textContent = lowPrice;
+	var img1 = document.getElementById("img1");
+	img1.setAttribute('src', resultArray[0].photos[0].url);
+	console.log(resultArray);
 
-var price2 = document.getElementById("price2");
-price2.textContent = highPrice;
+	var img2 = document.getElementById("img2");
+	img2.setAttribute('src', resultArray[1].photos[0].url);
 
-var img1 = document.getElementById("img1");
-img1.textContent = data[l].photos[0].url;
+	var info1 = document.getElementById("info1");
+	info1.textContent = resultArray[0].description;
 
-var img2 = document.getElementById("img2");
-img2.textContent = data[h].photos[0].url;
-
-var info1 = document.getElementById("info1");
-info1.textContent = data[l].description;
-
-var info2 = document.getElementById("info2");
-info2.textContent = data[h].description;
+	var info2 = document.getElementById("info2");
+	info2.textContent = resultArray[1].description;*/
+			
+										
 
 }
 
